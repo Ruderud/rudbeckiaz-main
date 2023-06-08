257b393e32b460dd03601da6503261e8c0db1819
정경훈 <ruderud0055@gmail.com>
2023-06-08 14:14:35 +0900

    feat: 메인페이지 상단바, 다크모드 기능 추가

---
+++ src/components/DarkModeButton.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { Moon, Sun } from './icons';
+
+export const DarkModeButton = () => {
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+
+  const changeDeviceDarkMode = (event: MediaQueryListEvent) => setIsDarkMode(event.matches);
+  const toggleDarkMode = () => setIsDarkMode(!isDarkMode);
+
+  useEffect(() => {
+    const htmlElement = document.querySelector('html');
+    if (isDarkMode) {
+      htmlElement?.classList.add('dark');
+    } else {
+      htmlElement?.classList.remove('dark');
+    }
+  }, [isDarkMode]);
+
+  useEffect(() => {
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+    setIsDarkMode(prefersDarkMode.matches);
+    prefersDarkMode.addEventListener('change', changeDeviceDarkMode);
+
+    return () => {
+      prefersDarkMode.removeEventListener('change', changeDeviceDarkMode);
+    };
+  }, []);
+
+  return (
+    <button className="bg-transparent text-black dark:text-slate-400" onClick={toggleDarkMode}>
+      {isDarkMode ? <Moon /> : <Sun />}
+    </button>
+  );
+};
