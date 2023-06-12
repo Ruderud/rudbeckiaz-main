b95693265e28804b10f307c6f113b246ce8688ab
정경훈 <ruderud0055@gmail.com>
2023-06-12 11:08:57 +0900

    feat: about 수행내역 리스트 작성

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -20,7 +20,7 @@ export default function AboutPage() {
           <p>좋은 동료들과 중요한 문제를 해결하며 임팩트 있는 제품을 만들어 나가고 싶습니다.</p>
         </article>
 
-        <div>
+        <div className="pl-6">
           <p>
             📧 Email:{' '}
             <Link className="text-blue-400 font-bold" href="mailto:ruderud0055@gmail.com">
@@ -43,20 +43,41 @@ export default function AboutPage() {
 
         <article className="pb-10">
           <p className="text-xl font-bold">🏢 Three ducks</p>
-          <p className="text-sm text-slate-300 pb-5">2023. 1. - 2023. 5. | Web Engineer (Intern)</p>
+          <p className="text-sm text-black dark:text-slate-300 pb-5">2023. 1. - 2023. 5. | Web Engineer (Intern)</p>
 
-          <div className="pl-6">
+          <div className="pl-6 pb-5">
             <p className="text-lg font-bold">Stari Web</p>
             <p className="text-md">
               <Link className="text-blue-400 font-bold" href="https://stari.io" target="_blank">
                 Stari
               </Link>
-              는 300여명 이상의 다양한 스타와 그들의 팬들을 영상을 통해 연결해주는 플랫폼입니다. 신규 스타 등록 및
-              고객의 영상 구입, 신규 기능 추가, 유지보수등의 운영업무 전반을 담당합니다. 신규 프로모션의 경우,
-              디자이너와의 협업을 통해 보다 나은 사용자 경험을 제공하여 많은 주문을 유도하였습니다.
+              는 300여명 이상의 다양한 스타와 그들의 팬들을 영상을 통해 연결해주는 플랫폼입니다.
             </p>
-            <ul>
-              <li></li>
+            <p className="text-md">
+              신규 스타 등록 및 고객의 영상 구입, 신규 기능 추가, 유지보수등의 운영업무 전반을 담당했습니다.
+            </p>
+            <p className="text-md"></p>
+
+            <ul className="pl-6 pt-5 list-disc flex flex-col gap-y-4 ">
+              <li>Lagacy Order API Migration: </li>
+              <li>신규 프로모션 릴리즈</li>
+              <li>비디오 UI 리뉴얼 릴리즈</li>
+              <li>주문 페이지 리뉴얼 릴리즈</li>
+            </ul>
+          </div>
+
+          <div className="pl-6">
+            <p className="text-lg font-bold">Stari Admin Tool</p>
+            <p className="text-md">
+              Stari에서의 스타 신규 계약 및 관리, 고객 주문사항 관리, 정산 관련 내용을 다루는 관리자 웹
+              어플리케이션입니다.
+            </p>
+
+            <ul className="pl-6 pt-5 list-disc flex flex-col gap-y-4">
+              <li>신규 스타 가입 폼 리뉴얼 릴리즈</li>
+              <li>JS to TS Migration</li>
+              <li>주문 내역 검색기능 릴리즈</li>
+              <li>i18n 릴리즈</li>
             </ul>
           </div>
         </article>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 import { TopBar } from '@/components';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,6 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="dark">
+      <Head>
+        <title>Rud</title>
+        <meta name="og:title" content="width=device-width, initial-scale=1" />
+      </Head>
       <body className={`${inter.className} min-h-screen bg-white dark:bg-slate-800 text-black dark:text-white`}>
         <TopBar />
         {children}
